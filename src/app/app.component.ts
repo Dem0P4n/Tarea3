731b6963d9ab1b04f2126ef62d53165479545400
Dem0P4n <ch.avendano@duocuc.cl>
2022-08-27 18:51:18 -0400

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tarea3';
+  public color: string = "#FFFFFF";
+  public colorLetras: string = "#F5C7A9";
+  public colorFondo: string = "#100720"
+  public capturarColor(event: Event): void{
+    const color = (event.target as HTMLInputElement).value
+    this.color = color;
+  }
+  public capturarColorLetras(event: Event):void{
+    const colorLetras = (event.target as HTMLInputElement).value
+    this.colorLetras = colorLetras;
+  }
+  public capturarColorFondo(event: Event):void{
+    const colorFondo = (event.target as HTMLInputElement).value
+    this.colorFondo = colorFondo;
+  }
+
 }
